dd772d0cb7cf7a41e7ba772f02939a50d274b82c
Ifja <Ifja@users.noreply.github.com>
2022-04-21 13:07:06 +0530

    Time: 2 ms (80.41%), Space: 41.4 MB (74.33%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        
+        for(int i=0;i<s.length();i++){
+            Character c=s.charAt(i);
+            if(c=='(' || c=='{' || c=='[')
+                st.push(c);
+            else if(c==')' || c=='}' || c==']'){
+                if(st.empty()) return false;
+                if ((c==')' && st.peek()=='(') ||
+                    (c=='}' && st.peek()=='{') ||
+                    (c==']' && st.peek()=='[')) st.pop();
+                else return false;
+            }
+        }
+        return st.empty()? true:false;
+        
+    }
+}
